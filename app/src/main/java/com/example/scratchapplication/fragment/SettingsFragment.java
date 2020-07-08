ba3f05b2a59496607624bade5575b08f274c18ce
nghiemson <ongnghiemsau@gmail.com>
2020-07-08 13:58:22 +0700

    update 8/7

---
+++ app/src/main/java/com/example/scratchapplication/fragment/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.example.scratchapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.scratchapplication.R;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+            addPreferencesFromResource(R.xml.pref_settings);
+
+    }
+}
